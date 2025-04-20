df5393767059ed71d5bef347848eab4b7105364f
Pranay Kallepu <pranaykallepu05@gmail.com>
2025-04-20 13:26:46 +0530

    @tailwindcss/postcss reinstalled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ReduxProvider from "@/components/ReduxProvider";
 import Navbar from "@/components/Navbar";
@@ -7,7 +7,15 @@ import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ["latin"] });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "FreshBulk App",
@@ -19,16 +27,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <ClerkProvider>
       <html lang="en" suppressContentEditableWarning>
         <head>
           <link rel="icon" href="/logo.ico" />
         </head>
-        <body className={inter.className}>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
           <div className="min-h-svh flex flex-col">
             <Navbar />
             <Toaster position="top-right" reverseOrder={false} />
